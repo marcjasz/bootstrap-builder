cadc7904c35f68524f89424eecbbe7467c7a3ecb
Marcin Jachymski <marcin.jachymski@bitgamelabs.com>
2019-12-29 22:49:21 +0100

    remove unneeded variables

--- src/main/java/pl/put/poznan/builder/logic/TemplateBuilder.java
+++ src/main/java/pl/put/poznan/builder/logic/TemplateBuilder.java
@@ -13,18 +13,6 @@ public class TemplateBuilder {
         private Element head;
         private Element body;
         private Element main;
-        private boolean headerEnable = false;
-        private String headerType;
-        private String headerText;
-        private boolean footerEnable = false;
-        private String footerText;
-        private String metaTitle;
-        private String metaType;
-        private String metaDescription;
-        private String metaImage; //?
-        private boolean metaSeoNormal;
-        private boolean metaSeoTwitter;
-        private boolean metaSeoOpengraph;
 
         /** Empty {@code <head>} element. */
         public Builder setHead() {
@@ -88,66 +76,6 @@ public class TemplateBuilder {
             }
             p.addAttribute(attr);
             this.main.addNode(p);
-            return this;
-        }
-
-        public Builder headerEnable(boolean headerEnable){
-            this.headerEnable = headerEnable;
-            return this;
-        }
-
-        public Builder headerType(String headerType){
-            this.headerType = headerType;
-            return this;
-        }
-
-        public Builder headerText(String headerText){
-            this.headerText = headerText;
-            return this;
-        }
-
-        public Builder footerEnable(boolean footerEnable){
-            this.footerEnable = footerEnable;
-            return this;
-        }
-
-        public Builder footerText(String footerText){
-            this.footerText = footerText;
-            return this;
-        }
-
-        public Builder metaTitle(String metaTitle){
-            this.metaTitle = metaTitle;
-            return this;
-        }
-
-        public Builder metaType(String metaType){
-            this.metaType = metaType;
-            return this;
-        }
-
-        public Builder metaDescription(String metaDescription) {
-            this.metaDescription = metaDescription;
-            return this;
-        }
-
-        public Builder metaImage(String metaImage){
-            this.metaImage = metaImage;
-            return this;
-        }
-
-        public Builder metaSeoNormal(boolean metaSeoNormal){
-            this.metaSeoNormal = metaSeoNormal;
-            return this;
-        }
-
-        public Builder metaSeoTwitter(boolean metaSeoTwitter){
-            this.metaSeoTwitter = metaSeoTwitter;
-            return this;
-        }
-
-        public Builder metaSeoOpengraph(boolean metaSeoOpengraph){
-            this.metaSeoOpengraph = metaSeoOpengraph;
             return this;
         }
 
